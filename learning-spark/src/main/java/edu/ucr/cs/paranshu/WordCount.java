0a1f6cf788c8b02295ba5b8d845c072b14e632ad
Paranshu Singhal <paranshu.singhal@gmail.com>
2019-11-15 22:15:05 -0800

    word frequency to avg word frequency

--- learning-spark/src/main/java/edu/ucr/cs/paranshu/WordCount.java
+++ learning-spark/src/main/java/edu/ucr/cs/paranshu/WordCount.java
@@ -6,16 +6,19 @@ import scala.Tuple2;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.util.LongAccumulator;
 import org.apache.spark.api.java.function.FlatMapFunction;
 
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.SparkContext;
+import org.apache.spark.Accumulator;
+import org.apache.spark.SparkConf;
 
 import java.util.Arrays;
 import java.util.Iterator;
-
-import org.apache.spark.SparkConf;
-
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * WordCount
@@ -27,42 +30,58 @@ public class WordCount {
         SparkConf conf = new SparkConf().setAppName("spark").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        countWords(args, sc);
+        
+    }
+
+    static void countWords(String[] args, JavaSparkContext sc){
+
+        final Accumulator<Integer> count = sc.accumulator(0);
+
         JavaRDD<String> rdd = sc.textFile(args[0]);
 
-        JavaPairRDD<String, Integer> pairRdd = rdd
+        JavaPairRDD<String, Double> pairRdd = rdd
         .flatMap(new FlatMapFunction<String,String>() {
             @Override
             public Iterator<String> call(String t) throws Exception {
                 return Arrays.asList(t.split(" ")).iterator();
             }
         })
-        .mapToPair(new PairFunction<String,String,Integer>() {
+        .mapToPair(new PairFunction<String,String,Double>() {
             @Override
-            public Tuple2<String,Integer> call(String t) throws Exception {
-                return new Tuple2<String,Integer>(t.replaceAll("\\p{Punct}", " ").toLowerCase().trim(),1);
+            public Tuple2<String,Double> call(String t) throws Exception {
+                count.add(1);
+                return new Tuple2<String,Double>(t.replaceAll("\\p{Punct}", " ").toLowerCase().trim(),(double)1);
             }
         })
-        .reduceByKey(new Function2<Integer,Integer,Integer>() {
-            @Override
-            public Integer call(Integer v1, Integer v2) throws Exception {
-                return v1+v2;
-            }
-        })
-        .mapToPair(new PairFunction<Tuple2<String,Integer>,Integer,String>() {
+        .reduceByKey(new Function2<Double,Double,Double>() {
 
             @Override
-            public Tuple2<Integer, String> call(Tuple2<String, Integer> t) throws Exception {
+            public Double call(Double v1, Double v2) throws Exception {
+                return (v1.doubleValue() + v2.doubleValue());
+            }
+        })
+        .mapToPair(new PairFunction<Tuple2<String,Double>,Double,String>() {
+
+            @Override
+            public Tuple2<Double, String> call(Tuple2<String, Double> t) throws Exception {
                 return t.swap();
             }
         })
         .sortByKey(false)
-        .mapToPair(new PairFunction<Tuple2<Integer,String>,String,Integer>() {
+        .mapToPair(new PairFunction<Tuple2<Double,String>,String,Double>() {
             @Override
-            public Tuple2<String, Integer> call(Tuple2<Integer, String> t) throws Exception {
+            public Tuple2<String, Double> call(Tuple2<Double, String> t) throws Exception {
                 return t.swap();
             }
         });
-        pairRdd.saveAsTextFile(args[1]);
+
+        List<Tuple2<String, Double>> lineList = new ArrayList();
+        for(Tuple2<String, Double> tuple: pairRdd.collect()){
+            lineList.add(new Tuple2<String, Double> (tuple._1(), tuple._2()/(count.value().doubleValue())));
+        }
+        sc.parallelize(lineList).saveAsTextFile(args[1]); 
+
     }
 
 }
